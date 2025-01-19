a823a3f9944f7c7badf61ba9ec4b0aaabe6f6212
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-19 22:31:21 +0900

    Generate a jti id on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  before_create :set_jti
+
+  private
+
+  def set_jti
+    self.jti ||= SecureRandom.uuid
+  end
 end
